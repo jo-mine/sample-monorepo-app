7d68b877a1f6aa2dcc1efa51267f4c9ea3459de8
jo-mine <minetab25@gmail.com>
2026-02-17 13:15:51 +0000

    submoduleの型をimport

--- packages/piyo-frontend/app/app.vue
+++ packages/piyo-frontend/app/app.vue
@@ -8,10 +8,17 @@
 import type { App } from "@jo-mine/piyo-backend";
 import { hoge } from "@jo-mine/sample-micro-services-hoge";
 import { hc } from "hono/client";
+import type { App as SubmoduleApp } from "@jo-mine/anon-backend";
 
 console.log(hoge());
 const client = hc<App>("http://localhost:3001");
 client.users.$get().then((res) => {
+  res.json().then((data) => {
+    console.log(data);
+  });
+});
+const submoduleClient = hc<SubmoduleApp>("http://localhost:3002");
+submoduleClient.news.$get().then((res) => {
   res.json().then((data) => {
     console.log(data);
   });
